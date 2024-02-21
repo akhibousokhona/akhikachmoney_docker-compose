1f7edba587c7869b26190ca7a0b6b1fddd38d783
akhibousokhona <85193164+akhibousokhona@users.noreply.github.com>
2024-02-21 16:05:09 +0100

    Update settings.py

--- connexion/connexion/settings.py
+++ connexion/connexion/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.getenv('DEBUG', False)
-import os
+
 #ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
