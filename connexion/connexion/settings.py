e9736b625337e1a1a2d2009c25b48e89b49c4615
akhibousokhona <85193164+akhibousokhona@users.noreply.github.com>
2024-02-21 16:16:58 +0100

    Update settings.py

--- connexion/connexion/settings.py
+++ connexion/connexion/settings.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+import json
+import logging.config
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
