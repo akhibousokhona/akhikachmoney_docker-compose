c0bef3b8d74640d0fbb2e65b7507171ca7a54150
akhibousokhona <akhibou1sokhona@gmail.com>
2024-04-10 14:29:22 +0200

    env database

--- connexion/connexion/settings.py
+++ connexion/connexion/settings.py
@@ -83,24 +83,13 @@ WSGI_APPLICATION = 'connexion.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 ###########
-#DATABASES = {
- # 'default': {
-  #  'ENGINE': 'django.db.backends.postgresql',
-  #  'HOST': os.environ.get('DB_HOST'),
-  #  'NAME': os.environ.get('DB_NAME'),
-  #  'USER': os.environ.get('DB_USER'),
-  #  'PASSWORD': os.environ.get('DB_PASS'),
-  #  'PORT': os.environ.get('DB_PORT'),
- # }
-#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'akhikachmoney',
-        'USER': 'moussa',
-        'PASSWORD': 'passer',
-        'HOST': 'db',
-        'PORT': '5432'
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 ###########
